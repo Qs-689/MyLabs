61a4867c613320cfdc84e44ee778b14524ac8f00
Qi <1654970286@qq.com>
2025-09-30 00:03:54 +0800

    demo3

---
+++ Lab2_LayOut2/app/src/main/java/com/example/lab2_layout/MainActivity1.java
@@ -0,0 +1,55 @@
+package com.example.lab2_layout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main1);
+
+        setupBottomNavigation();
+    }
+    private void setupBottomNavigation() {
+        Button btn1 = findViewById(R.id.btn_activity1);
+        Button btn2 = findViewById(R.id.btn_activity2);
+        Button btn3 = findViewById(R.id.btn_activity3);
+        Button btn4 = findViewById(R.id.btn_activity4);
+
+        // 当前Activity的按钮设置为不可点击状态
+        btn1.setEnabled(false);
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity1.this, MainActivity2.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity1.this, MainActivity3.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity1.this, MainActivity4.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
---
+++ Lab2_LayOut2/app/src/main/java/com/example/lab2_layout/MainActivity2.java
@@ -0,0 +1,55 @@
+package com.example.lab2_layout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        setupBottomNavigation();
+    }
+
+    private void setupBottomNavigation() {
+        Button btn1 = findViewById(R.id.btn_activity1);
+        Button btn2 = findViewById(R.id.btn_activity2);
+        Button btn3 = findViewById(R.id.btn_activity3);
+        Button btn4 = findViewById(R.id.btn_activity4);
+
+        // 当前Activity的按钮设置为不可点击状态
+        btn2.setEnabled(false);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity2.this, MainActivity1.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity2.this, MainActivity3.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity2.this, MainActivity4.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
